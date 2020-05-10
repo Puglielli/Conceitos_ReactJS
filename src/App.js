e124ac14cc8e20af13c85bc0399c9b7354a29158
ppuglielli <puglielli14@gmail.com>
2020-05-10 00:26:46 -0300

    finalizacao do desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
+const SIDES = ['Front-end', 'Back-end', 'Mobile'];
+
+const COMPLEMENT = ['Weather', 'Calculator', 'API Rest', 'Rest', 'Locadora', 'Comanda'];
+
+const TECHS = ['React', 'ReactJS', 'React Native', 'Java', 'Node.js'];
+
+const REPOSITORIES = [
+  'Rocketseat/bootcamp-gostack-desafios', 'Puglielli/bootcamp-gostack-desafios'];
+
+function createNewRepository() {
+  const title = `${SIDES[Math.round(Math.random() * (SIDES.length - 1))]} - `
+              + `${COMPLEMENT[Math.round(Math.random() * (COMPLEMENT.length - 1))]} `;
+
+  const url = `https://github.com/${REPOSITORIES[Math.round(Math.random() * (REPOSITORIES.length - 1))]}`;
+
+  const techs = TECHS[Math.round(Math.random() * (TECHS.length - 1))];
+  
+  return {
+    title,
+    url,
+    techs
+  };
+}
+
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', createNewRepository());
+    
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+   const response = await api.delete(`repositories/${id}`);
+   
+   if (response.status !== 204) return;
+    
+   const projectIndex = projects.findIndex(project => project.id === id);
+
+   projects.splice(projectIndex, 1);
+
+   setProjects([...projects]);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+      {projects.map(project => <li key={project.id}>{project.title}
+      
+        <button onClick={() => handleRemoveRepository(project.id)}>
             Remover
           </button>
-        </li>
+      </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
